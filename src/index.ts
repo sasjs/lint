c88aa8b3f63f0e7f4725128b43e4fce1c8bcf3eb
Krishna Acondy <kc.acondy@outlook.com>
2021-03-24 16:51:36 +0000

    fix(*): export types from package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { lint } from './lint'
+export * from './types'
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from './Diagnostic'
+export * from './LintConfig'
+export * from './LintRule'
+export * from './LintRuleType'
+export * from './Severity'
