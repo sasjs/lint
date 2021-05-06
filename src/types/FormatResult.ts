e3295294842fd50bb463dc5eac9930c645854f31
Krishna Acondy <kc.acondy@outlook.com>
2021-05-06 07:24:07 +0100

    chore(*): add comment

--- src/types/FormatResult.ts
+++ src/types/FormatResult.ts
@@ -1,5 +1,8 @@
 import { Diagnostic } from './Diagnostic'
 
+/**
+ * Represents the result of a format operation on a file, folder or project.
+ */
 export interface FormatResult {
   updatedFilePaths: string[]
   fixedDiagnosticsCount: number
